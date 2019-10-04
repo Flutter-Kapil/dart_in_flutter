7b795f91cd0e933817e3f88ab7285b0011b24381
kapil jhajhria <jhajhria44@gmail.com>
2019-10-04 21:16:32 +0530

    Logic for simple calculator app.

--- bin/main.dart
+++ bin/main.dart
@@ -1,24 +1,36 @@
-import 'package:english_words/english_words.dart';
 import 'dart:math';
+import 'package:petitparser/petitparser.dart';
 
 void main() {
-  print('--------');
-  generateWordPairs().take(1).forEach(print);
+  final builder = ExpressionBuilder();
 
-  var r = WordPair.random(random: Random()).asCamelCase;
-  print(r);
-//  print(all);
-  String z = "";
-  for (int i = 0; i < 3; i++) {
-    int y = Random().nextInt(all.length);
-    String rWord = all[y];
-    if (i == 0) {
-      z = z + ' ' + rWord;
-    } else {
-      z = z + capitalize(rWord);
-    }
-  }
-  //print(z);
+  builder.group()
+    ..primitive(digit()
+        .plus()
+        .seq(char('.').seq(digit().plus()).optional())
+        .flatten()
+        .trim()
+        .map((a) => num.tryParse(a)))
+    ..wrapper(char('(').trim(), char(')').trim(), (l, a, r) => a);
+
+  // negation is a prefix operator
+  builder.group()..prefix(char('-').trim(), (op, a) => -a);
+
+// power is right-associative
+  var math;
+  builder.group()..right(char('^').trim(), (a, op, b) => math.pow(a, b));
+
+// multiplication and addition are left-associative
+  builder.group()
+    ..left(char('*').trim(), (a, op, b) => a * b)
+    ..left(char('/').trim(), (a, op, b) => a / b);
+  builder.group()
+    ..left(char('+').trim(), (a, op, b) => a + b)
+    ..left(char('-').trim(), (a, op, b) => a - b);
+
+  final parser = builder.build().end();
+  String fromButtons = ""; // add all the strings as soon as user taps on = and pass it
+  //to below function for solution
+  var finalOutput = parser.parse('24*(2)');
+  print(finalOutput);
 }
-
-String capitalize(String s) => s[0].toUpperCase() + s.substring(1);
