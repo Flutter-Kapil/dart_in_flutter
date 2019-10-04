59720b15dc455bc9f5a97c783fed7a8a1564c66c
kapil jhajhria <jhajhria44@gmail.com>
2019-10-04 20:40:56 +0530

    random words in camel case.

--- bin/main.dart
+++ bin/main.dart
@@ -1,60 +1,24 @@
-// In these exercises we will pretend that we do not have access to the standard
-// List class in Dart. Instead, we will implement our own list using 'links' as
-// described below.
+import 'package:english_words/english_words.dart';
+import 'dart:math';
 
-// Exercise 1: Create a class called 'Link' which has two fields.  Choose
-// types for each field so that you can create an object structure that looks
-// like this:
-// https://s1.whiteboardfox.com/s/2ae37dc832647bad.png
+void main() {
+  print('--------');
+  generateWordPairs().take(1).forEach(print);
 
-class Link {
-  var next;
-  var value;
-}
-
-main() {
-  // Exercise 2:  Create the object structure shown in the above link
-  // including the variable called 'firstLink'
-  Link firstLink = Link();
-  firstLink.value = 'Raj';
-  firstLink.next = Link();
-  firstLink.next.value = 'Makiko';
-  firstLink.next.next = Link();
-  firstLink.next.next.value = 'Paul';
-
-  printFirstLink(firstLink);
-  print('----------');
-  String y = 'kapil';
-  add2FirstLink(firstLink, y);
-}
-
-// Exercise 3: Create a function which takes a Link parameter and prints out
-// all the values in the list.  For example, if the function is given the
-// firstLink variable, then it will print:
-// Raj
-// Makiko
-// Paul
-void printFirstLink(Link x) {
-  while (x != null) {
-    print(x.value);
-    x = x.next;
+  var r = WordPair.random(random: Random()).asCamelCase;
+  print(r);
+//  print(all);
+  String z = "";
+  for (int i = 0; i < 3; i++) {
+    int y = Random().nextInt(all.length);
+    String rWord = all[y];
+    if (i == 0) {
+      z = z + ' ' + rWord;
+    } else {
+      z = z + capitalize(rWord);
+    }
   }
+  //print(z);
 }
 
-// Exercise 4: Create a function which takes a Link parameter and a String
-// parameter, and adds another link to the end of the chain.  The new link
-// value should be the String parameter passed into the function.typedef
-void add2FirstLink(Link x, String z) {
-  while (x != null) {
-    x = x.next;
-  }
-  x = Link();
-  x.value = z;
-  print(x.value);
-}
-// *** Stretch ****
-// Exercise 5: Create a function called insert which takes a Link,
-// an integer index and a String, and inserts a link at the given index
-// in the chain.  For example, calling insert(firstLink, 2, "A") should
-// update the chain structure to look like this:
-// https://s1.whiteboardfox.com/s/77ff03ee231f2f60.png
+String capitalize(String s) => s[0].toUpperCase() + s.substring(1);
